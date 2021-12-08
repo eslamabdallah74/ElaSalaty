836927248e37ec443a680c36637bbc1074072652
eslamabdallah74 <eslamabdallah301@gmail.com>
2021-12-08 03:27:47 +0200

    make buttons disabed upon clicking

--- app/Http/Livewire/Prayers.php
+++ app/Http/Livewire/Prayers.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Exp_bar;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
+
 use App\Models\Prayers as ModelsPrayers;
 
 class Prayers extends Component
@@ -17,11 +18,13 @@ class Prayers extends Component
     public $level;
     public $ExpBar;
     public $oldExpBar;
-
+    public $ButtonClicked;
     public $GetExp;
 
+    public $failed;
     public function mount()
     {
+
         // Make new exp row for new user
         $IsExpExists = Exp_bar::where('user_id', auth()->user()->id)->first();
         if ($IsExpExists === null) {
@@ -34,37 +37,47 @@ class Prayers extends Component
         }
         // Assgin Values
         $this->prayers = ModelsPrayers::OrderBy('id','desc')->get();
-        $this->GetExp = Exp_bar::where('user_id', auth()->user()->id)->get('exp')->first();
-        $this->level  = Exp_bar::where('user_id', auth()->user()->id)->get('level')->first();
+        $this->GetExp  = Exp_bar::where('user_id', auth()->user()->id)->get('exp')->first();
+        $this->level   = Exp_bar::where('user_id', auth()->user()->id)->get('level')->first();
+
 
     }
 
-    public function Prayed()
+    public function Prayed($prayer_id)
     {
         auth()->check();
-
-            // Add Exp
+        // if button didn't been clicked in 24h you can click it
+        if(ModelsPrayers::where('id' , $prayer_id)->value('clicked') === 0)
+        {
+             // Add Exp and clicked
              $UpdateExp = Exp_bar::where('user_id', auth()->user()->id)->get()->first()->update([
-                 'exp' =>  DB::raw('exp+5')
-             ]);
-                // Here we update the (this exp so we can live render it)
-             $this->GetExp = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
+                'exp'      =>  DB::raw('exp+5'),
+            ]);
+           // button Clicked
+            $Clicked = ModelsPrayers::where('id' , $prayer_id)->first()->update([
+               'clicked'  => '1',
+            ]);
+               // Here we update the (this exp so we can live render it)
+            $this->GetExp = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
 
-            //  Level up
-             if($this->GetExp->exp >= 100)
-             {
-                $UpdateExp = Exp_bar::where('user_id', auth()->user()->id)->get()->first()->update([
-                    'exp'   =>  0,
-                    'level' => DB::raw('level+1')
-                ]);
-                // Update on level up
-                $this->GetExp = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
-                $this->level = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
+           //  Level up
+            if($this->GetExp->exp >= 100)
+            {
+               $UpdateExp = Exp_bar::where('user_id', auth()->user()->id)->get()->first()->update([
+                   'exp'   =>  0,
+                   'level' => DB::raw('level+1')
+               ]);
+               // Update on level up
+               $this->GetExp = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
+               $this->level = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
 
-             }
-            // Here we update the (this level so we can live render it)
-            $this->level = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
-
+            }
+           // Here we update the (this level so we can live render it)
+           $this->level    = Exp_bar::where('user_id', auth()->user()->id)->get()->first();
+        } else { // you clicked the pray
+            $this->failed = 'You prayed it already';
+            return $this->failed;
+        }
 
     }
     public function render()
--- resources/views/livewire/prayers.blade.php
+++ resources/views/livewire/prayers.blade.php
@@ -1,16 +1,21 @@
 <div class="py-4" style="margin: 40px 0;">
+
     <div class="container">
         <div class="row">
             <div class="col-md-2" style="font-size: 18px;"> <span class="level"> {{ $level->level }} </span> المستوي </div>
             @foreach ($prayers as $prayer )
             <div class="py-2 col-md-2">
                 <div class="btn-contain">
-		               <button  wire:click='Prayed()'  class="col btn btn-light-moon"> {{ $prayer->pray_name }} </button>
+		               <button
+                        @if ($prayer->clicked === 1)
+                            disabled
+                        @endif
+                        wire:click='Prayed({{$prayer->id}})'  class="col btn btn-light-moon"}> {{ $prayer->pray_name  }} </button>
                 </div>
             </div>
             @endforeach
         </div>
-
+        <h5 class="py-2" style="color: #ff4d4d;"> {{ $failed }} </h5>
     </div>
     <div class="container">
         <div class="row">
